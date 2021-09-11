06ba2abe42e474f246ca3825ce5cbc0f02907092
Adrian Vinícius Roma Alves <75907345+adrian0-0@users.noreply.github.com>
2021-09-11 20:28:22 -0300

    Simple Configuration

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+import java.security.SecureRandom;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello Sudoku!");
+        System.out.println(Color.ANSI_PURPLE + "Hello Sudoku!" + Color.ANSI_RESET);
+        Scanner input = new Scanner(System.in);
+        final int linhas = 3;
+        final int colunas = 3;
+        int[][] numeros=new int[linhas][colunas];  
+        
+        for (int l=0; l<linhas; l++) {
+        	for (int c = 0; c<colunas; c++) {
+        		numeros[l][c]= new SecureRandom().nextInt(9);
+        		System.out.printf("%2d |", numeros[l][c]);
+        	}
+        	System.out.printf("%n");
+        }
     }
 }
